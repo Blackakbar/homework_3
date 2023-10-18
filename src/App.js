415ac1938738e3aea4232aa54ea5cc36c729d753
Blackakbar <akbar04222@gmail.com>
2023-10-18 12:09:57 +0600

    homework_3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class TaskToDo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [],
+      currentTask: '',
+    };
+  }
+
+  addTask = () => {
+    const { currentTask } = this.state;
+    if (currentTask.trim() !== '') {
+      this.setState((prevState) => ({
+        tasks: [...prevState.tasks, currentTask],
+        currentTask: '', 
+      }));
+    }
+  }
+
+  deleteTask = (index) => {
+    this.setState((prevState) => ({
+      tasks: prevState.tasks.filter((_, i) => i !== index),
+    }));
+  }
+
+  handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      this.addTask();
+    }
+  }
+
+  render() {
+    const { tasks, currentTask } = this.state;
+
+    return (
+      <div className='wrapper'>
+        <div className='task-manager-block'>
+          <div className='container'>
+            <div className='task-list'>
+              {tasks.map((task, index) => (
+                <div key={index} className='task'>
+                  <input type='checkbox' />
+                  {task}
+                  <button className='delete-task' onClick={() => this.deleteTask(index)}>Удалить</button>
+                </div>
+              ))}
+            </div>
+            <div className='task-manager'>
+              <input
+                type='text'
+                value={currentTask}
+                onChange={(e) => this.setState({ currentTask: e.target.value })}
+                onKeyPress={this.handleKeyPress}
+                placeholder='Введите задачу...'
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default TaskToDo;
+
+
